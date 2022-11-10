ffc1d00248d7bc46bcac69dcc2d41ddf64379aec
Max Melnyk <max@maxmelnyk.dev>
2022-11-09 20:25:19 -0500

    use saas

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,11 +5,35 @@
     </h1>
     <div class="fs-5 text-color-red">Software Engineer</div>
     <div class="fs-6">
-      <i class="bi bi-telephone-fill align-middle"></i> +14168541631
-      <span class="mx-3">|</span>
-      <i class="bi bi-envelope-fill align-middle"></i> max@maxmelnyk.dev
-      <span class="mx-3">|</span>
-      <i class="bi bi-github align-middle"></i> imaxmelnyk
+      <span class="contact"><i class="bi bi-telephone-fill align-middle"></i> +14168541631</span>
+      <span class="mx-3 separator">|</span>
+      <span class="contact"><i class="bi bi-envelope-fill align-middle"></i> max@maxmelnyk.dev</span>
+      <span class="mx-3 separator">|</span>
+      <span class="contact"><i class="bi bi-github align-middle"></i> imaxmelnyk</span>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import "../../node_modules/bootstrap/scss/functions";
+@import "../../node_modules/bootstrap/scss/variables";
+@import "../../node_modules/bootstrap/scss/mixins";
+
+.separator {
+  display: none;
+}
+
+.contact {
+  display: block;
+}
+
+@include media-breakpoint-up(sm) {
+  .separator {
+    display: inline;
+  }
+
+  .contact {
+    display: inline;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import './assets/main.css'
+import './assets/main.scss'
 
 createApp(App).mount('#app')
