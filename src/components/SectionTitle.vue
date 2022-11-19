a228873b1b89c619a07a511a166df3562467e85d
Max Melnyk <max@maxmelnyk.dev>
2022-11-19 12:11:23 -0500

    fix baseline draw

--- src/components/SectionTitle.vue
+++ src/components/SectionTitle.vue
@@ -8,14 +8,17 @@ defineProps({
 </script>
 
 <template>
-  <div class="fs-2">
+  <div class="fs-2 mb-1 d-flex">
     <span class="text-color-red">{{ title.substring(0, 3) }}</span><span class="fw-bold">{{ title.substring(3) }}</span>
   </div>
 </template>
 
 <style scoped>
-div {
-  border-bottom: 1px solid #ccc;
-  margin-bottom: 0.5em;
+div:after {
+  content: '';
+  flex: 1;
+  border-bottom: 0.075em solid #444;
+  margin-left: 0.15em;
+  margin-bottom: 0.375em;
 }
 </style>
