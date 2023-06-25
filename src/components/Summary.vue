353b01091befb2996d3977a020143331ef7c6d4c
Max Melnyk <max@maxmelnyk.dev>
2023-06-24 21:23:35 -0400

    Update summary

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -8,7 +8,7 @@ import Section from './Section.vue'
       <p>
         As a software engineer with 8 years of experience, I specialize in designing and building scalable,
         fault-tolerant backend systems and distributed architectures that can handle large volumes of traffic and data.
-        I have a strong foundation in functional programming principles and
+        I have a strong foundation in object oriented and functional programming principles and
         am well-versed in working with a variety of programming languages and technologies.
       </p>
       <p>
